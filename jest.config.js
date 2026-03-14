b673ef10055cf7c7e267b5aa6ff364245801a9bf
HJLE <02q09q@gmail.com>
2026-03-14 11:04:52 +0900

    fix: use standard react-native jest preset path

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,17 @@
+const rnPreset = require('react-native/jest-preset');
+
 module.exports = {
+  ...rnPreset,
   collectCoverageFrom: [
     '<rootDir>/src/Components/**/*.{jsx, tsx}',
     '<rootDir>/src/App.{jsx, tsx}',
   ],
   coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  preset: 'react-native',
-  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+  setupFiles: [
+    ...rnPreset.setupFiles,
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testMatch: ['**/*.test.ts?(x)', '**/*.test.js?(x)'],
   transformIgnorePatterns: [
